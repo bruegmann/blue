feec051cdf6ee91c94cf11f98acbcf3bfd24befd
lgk-bsw <lgk@patorg.de>
2026-09-21 09:46:06 +0200

    fix: correct import case for Button component

--- apps/docs/src/components/CopyToClipboard.tsx
+++ apps/docs/src/components/CopyToClipboard.tsx
@@ -1,4 +1,4 @@
-import Button from "blue-react/dist/components/button.js"
+import Button from "blue-react/dist/components/Button.js"
 import { useEffect, useState, type ComponentProps } from "react"
 import { Clipboard, ClipboardCheck } from "react-bootstrap-icons"
 
